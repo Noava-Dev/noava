d1afdc541868d786da5fed37725fbefce8da93b4
brentvanroelen <145969821+brentvanroelen@users.noreply.github.com>
2026-01-21 14:20:32 +0100

    Fixed main.tsx wrapping

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -18,13 +18,13 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById('root')!).render(
   
   <StrictMode>
-    <ToastProvider>
-    <BrowserRouter>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+      <BrowserRouter>  
+          <ToastProvider>
+            <App />
+          </ToastProvider>
+      </BrowserRouter>
     </ClerkProvider>
-    </BrowserRouter>
-    </ToastProvider>
   </StrictMode>
 
 );
