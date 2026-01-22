fbdbcafa48a9b807975b5fbc5fa7ae46c61b735b
youmni <146707312+youmni@users.noreply.github.com>
2026-01-22 21:27:04 +0100

    Remove unused attribute import from FAQController

--- backend/noava/noava/Controllers/FAQController.cs
+++ backend/noava/noava/Controllers/FAQController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using noava.Attributes;
 using noava.Data;
 using noava.Models;
 using noava.Services;
