939a0bec14a31fc6adc513f179b3f617c525d2f3
TomeIDK <cecepas2001@gmail.com>
2026-01-25 13:15:00 +0100

    azure blob storage service

---
+++ backend/noava/noava/Models/Requests/SaveFileRequest.cs
@@ -0,0 +1,11 @@
+﻿using Azure.Storage.Blobs;
+
+namespace noava.Models.BlobStorage
+{
+    public class SaveFileRequest
+    {
+        public BlobContainerClient Container { get; set; } = null!;
+        public string BlobName { get; set; } = null!;
+        public IFormFile FormFile { get; set; } = null!;
+    }
+}
