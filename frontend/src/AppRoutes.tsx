5f1049d8caa3b94dde84ef5aa4967c30e124bfc0
brentvanroelen <145969821+brentvanroelen@users.noreply.github.com>
2026-01-26 09:57:10 +0100

    Update AppRoutes.tsx

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -2,7 +2,7 @@ import {Route, Routes} from "react-router-dom";
 import FAQ from "./pages/FAQ/FAQ";
 import Home from "./pages/Home/Home"
 import PrivateRoute from "./shared/components/navigation/PrivateRoute";
-import { RoleGroups } from "./models/User"; *
+import { RoleGroups } from "./models/User"; 
 
 export default function AppRoutes() {
     return (
