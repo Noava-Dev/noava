e81f474908ea04d1f6296a3e8ee304bd0f17014c
Tome <133920130+TomeIDK@users.noreply.github.com>
2026-01-26 10:08:40 +0100

    Blobservice (#79)
    * azure blob storage service
    
    * Update noava.csproj

---
+++ backend/noava/noava/Models/Requests/EnsureContainerRequest.cs
@@ -0,0 +1,10 @@
+﻿using Azure.Storage.Blobs.Models;
+
+namespace noava.Models.BlobStorage
+{
+    public class EnsureContainerRequest
+    {
+        public string ContainerName { get; set; } = null!;
+        public PublicAccessType AccessType { get; set; } = PublicAccessType.None;
+    }
+}
---
+++ backend/noava/noava/Models/Requests/GetFileSasRequest.cs
@@ -0,0 +1,11 @@
+﻿using Azure.Storage.Sas;
+
+namespace noava.Models.BlobStorage
+{
+    public class GetFileSasRequest
+    {
+        public string ContainerName { get; set; } = null!;
+        public string BlobName { get; set; } = null!;
+        public BlobSasPermissions Permission { get; set; } = BlobSasPermissions.Read;
+    }
+}
