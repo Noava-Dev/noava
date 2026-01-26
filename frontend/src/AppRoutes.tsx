cba23f5c2ec20f71f0a2e9f3d509884bb6b45271
brentvanroelen <145969821+brentvanroelen@users.noreply.github.com>
2026-01-26 09:56:47 +0100

    Update AppRoutes.tsx

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,17 +1,17 @@
 import {Route, Routes} from "react-router-dom";
 import FAQ from "./pages/FAQ/FAQ";
 import Home from "./pages/Home/Home"
-/* import PrivateRoute from "./shared/components/navigation/PrivateRoute";
-import { RoleGroups } from "./models/User"; */
+import PrivateRoute from "./shared/components/navigation/PrivateRoute";
+import { RoleGroups } from "./models/User"; *
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Home/>}/> 
             <Route path="/faq" element={<FAQ/>}/>
-            {/* <Route element={<PrivateRoute allowedRoles={RoleGroups.ALL_AUTHENTICATED} />}>
+             <Route element={<PrivateRoute allowedRoles={RoleGroups.ALL_AUTHENTICATED} />}>
                 <Route path="/dashboard" element={<Placeholder title="dashboard" />}/>
-            </Route> */}
+            </Route> 
         </Routes>
     );
 }
