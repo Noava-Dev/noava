9f90b548cd47a43aa53d75f2792679965768c348
Luna D'Heere <161327120+LunaDHeere@users.noreply.github.com>
2026-01-27 13:06:24 +0100

    Settings (#87)
    * ADD: made basic setup of the settingspage + added component for the buttons
    
    * FIX: all info is on page, styling still needed now
    
    * ADD: added styilng to the components
    
    * FIX: implemented grid for a better layout
    
    * FIX: fixed colors in dark mode
    
    * ADD: added TODO comments
    
    * DELETE: removed unused import
    
    * ADD: logic to change the theme of the website
    
    * ADD: logic to set theme in localStorage
    
    no logic is set yet to actually change the language. will be discussed in daily standup
    
    * UPDATE: made it so the settingspage changes languages
    
    * FIX: language is now changed globally across all pages
    
    * change lang selector to dropdown + theming
    
    ---------
    
    Co-authored-by: LunaDHeere05 <luna.dheere@student.ehb.be>
    Co-authored-by: TomeIDK <cecepas2001@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,46 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import AppRoutes from './AppRoutes';
 import { UserRoleProvider } from './contexts/UserRoleContext';
 import { SidebarNav } from './shared/components/Sidebar';
 import { useLocation } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
 
 function App() {
 const location = useLocation();
   
-  
   const routesWithoutSidebar = ['/', '/home', '/faq'];
   const showSidebar = !routesWithoutSidebar.includes(location.pathname);
 
+  useEffect(() => {
+    const applyTheme = () => {
+      const theme = localStorage.getItem("theme") || "light";
+      document.documentElement.classList.toggle("dark", theme === "dark");
+    };
+
+    applyTheme();
+    window.addEventListener("storage", applyTheme)
+    return () => {
+      window.removeEventListener("storage", applyTheme)
+    }
+  }, [])
+
+  //-------------- language ---------------------
+  const [language, setLanguage] = useState(() => {
+    return localStorage.getItem("preferredLanguage") || i18next.language || "en";
+  });
+
+  useEffect(() => {
+    i18next.changeLanguage(language);
+    localStorage.setItem("preferredLanguage", language);
+  }, [language]);
+
+
   return (
     <UserRoleProvider>
+      <I18nextProvider i18n={i18next}>
+
       <div className="flex h-screen">
         
         {showSidebar && (
@@ -25,6 +53,8 @@ const location = useLocation();
           <AppRoutes />
         </main>
       </div>
+      </I18nextProvider>
+
     </UserRoleProvider>
   );
 }
