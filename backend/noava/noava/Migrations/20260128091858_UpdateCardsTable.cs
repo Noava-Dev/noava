76bbe33512281cb3446d7ed1fb21c880242fc97c
Tome <cecepas2001@gmail.com>
2026-01-28 10:19:28 +0100

    Add CardConfiguration and Card props

---
+++ backend/noava/noava/Migrations/20260128091858_UpdateCardsTable.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace noava.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCardsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BackAudio",
+                table: "Cards",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BackImage",
+                table: "Cards",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BackText",
+                table: "Cards",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Cards",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "now()");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DeckId",
+                table: "Cards",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FrontAudio",
+                table: "Cards",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FrontImage",
+                table: "Cards",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FrontText",
+                table: "Cards",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Memo",
+                table: "Cards",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Cards",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "now()");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_DeckId",
+                table: "Cards",
+                column: "DeckId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cards_Decks_DeckId",
+                table: "Cards",
+                column: "DeckId",
+                principalTable: "Decks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cards_Decks_DeckId",
+                table: "Cards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cards_DeckId",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "BackAudio",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "BackImage",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "BackText",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "DeckId",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "FrontAudio",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "FrontImage",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "FrontText",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "Memo",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Cards");
+        }
+    }
+}
--- backend/noava/noava/Models/Card.cs
+++ backend/noava/noava/Models/Card.cs
@@ -3,5 +3,15 @@
     public class Card
     {
         public int Id { get; set; }
+        public int DeckId { get; set; }
+        public string FrontText { get; set; }
+        public string BackText { get; set; }
+        public string? FrontImage { get; set; }
+        public string? BackImage { get; set; }
+        public string? FrontAudio { get; set; }
+        public string? BackAudio { get; set; }
+        public string? Memo { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
 }
