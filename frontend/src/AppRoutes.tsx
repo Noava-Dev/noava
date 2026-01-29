b97486e214357d11f829100713de46aacc1e5fa0
Tome <cecepas2001@gmail.com>
2026-01-29 14:22:16 +0100

    remove components, add clerk components directly

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -4,22 +4,32 @@ import Home from './pages/Home/Home';
 import NotFound from './pages/NotFound/NotFound';
 import SettingsPage from './pages/Settings/Settings';
 import NoavaFooter from './shared/components/NoavaFooter';
+import { SignUp as SignUpClerk } from "@clerk/clerk-react";
+import { SignIn as SignInClerk } from "@clerk/clerk-react";
+import PrivateRoute from './shared/components/navigation/PrivateRoute';
+import { RoleGroups } from './models/User';
+import AdminDashboard from './pages/Admin/Dashboard/AdminDashboard';
+import Loading from './shared/components/Loading';
 
 function Placeholder({ title }: { title: string }) {
   return <NoavaFooter />;
 }
 
-import PrivateRoute from './shared/components/navigation/PrivateRoute';
-import { RoleGroups } from './models/User';
-import AdminDashboard from './pages/Admin/Dashboard/AdminDashboard';
-import SignIn from './pages/SignIn/SignIn';
-import SignUp from './pages/Register/Register';
+
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/sign-in" element={
+        <div className="min-h-screen flex items-center justify-center py-8">
+          <SignInClerk fallback={<Loading />} />
+        </div>
+        } />
+      <Route path="/sign-up" element={
+        <div className="min-h-screen flex items-center justify-center py-8">
+          <SignUpClerk fallback={<Loading />} />
+        </div>
+        } />
       <Route path="/" element={<Home />} />
       <Route path="/decks" element={<Placeholder title="Decks" />} />
       <Route path="/classrooms" element={<Placeholder title="Klassen" />} />
